146e847e91500ce951ab7c0418ae0da4efb7958a
Kiss Miklos <miklos@roadie.io>
2026-02-18 21:21:34 +0100

    fix
    Signed-off-by: Kiss Miklos <miklos@roadie.io>

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -3,6 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@backstage/ui/css/styles.css';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
